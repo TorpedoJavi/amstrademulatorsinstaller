85ba070a81d5c4653cfd6d43deef222fc59858e0
TorpedoJavi <116526180+TorpedoJavi@users.noreply.github.com>
2025-09-29 11:13:04 +0200

    Proposal Update vice_installer.sh

--- installers/vice_installer.sh
+++ installers/vice_installer.sh
@@ -1,17 +1,18 @@
 #/bin/bash
 #VICE Commodore Emulator
+#
+#Install depndencies
 sudo apt install -y xa65 p7zip build-essential byacc texi2html flex libreadline-dev libxaw7-dev texinfo libxaw7-dev libgtk2.0-cil-dev libgtkglext1-dev libpulse-dev wget
-wget -c https://downloads.sourceforge.net/project/vice-emu/releases/binaries/windows/GTK3VICE-3.4-win64.7z
-sudo chmod +rx /usr/share/lib/
-sudo 7zr x -o/usr/share/lib GTK3VICE-3.4-win64.7z GTK3VICE-3.4-win64-r37296
-sudo mv /usr/share/lib/GTK3VICE-3.4-win64-r37296 /usr/share/lib/vice
-wget -c https://downloads.sourceforge.net/project/vice-emu/releases/vice-3.4.tar.gz
-tar -zxvf vice-3.4.tar.gz
-cd vice-3.4
-./configure --with-x --without-pulse --with-sdlsound --enable-sdlui2
-make
-sudo make install
-cd ..
+#
+#Download latest VICE
+wget https://downloads.sourceforge.net/project/vice-emu/releases/binaries/debian/sdl2vice_3.9.deb
+wget https://downloads.sourceforge.net/project/vice-emu/releases/binaries/debian/gtk3vice_3.9.deb
+#
+#Install latest VICE
+sudo dpkg -i sdl2vice_3.9.deb
+sudo dpkg -i gtk3vice_3.9.deb
+#
+#Create shortcuts
 ../commonscripts/createshortcuts.sh x128
 ../commonscripts/createshortcuts.sh x64sc
 ../commonscripts/createshortcuts.sh x64dtv
